3b33291f3287b09c1a6b2857488abcf5b3cf221c
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2021-06-26 20:18:28 +0430

    Adding flatten and dense layers to vgg-16 in order final model

--- main/model.py
+++ main/model.py
@@ -17,3 +17,11 @@ vgg_model = VGG16(weights="imagenet", include_top=False, input_shape=(224, 244, 
 # Freezing the model layers except last block
 for layer in vgg_model.layers[:15]:
     layer.trainable = False
+
+# Adding flatten and dense layers to vgg-16 in order final model
+output = vgg_model.output
+output = Flatten()(output)
+output = Dense(512, activation=relu)(output)
+output = Dropout(0.5)(output)
+output = Dense(8, activation=softmax)(output)
+final_model = Model(inputs=vgg_model.inputs, outputs=output)
