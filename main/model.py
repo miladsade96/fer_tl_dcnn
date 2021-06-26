992b0f553c367d7a8b0adb0811d8fc875e93a86f
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2021-06-26 20:13:59 +0430

    Importing relevant modules

--- main/model.py
+++ main/model.py
@@ -1,3 +1,11 @@
 """
     Fine Tuning VGG-16 Pre-Trained Model
 """
+
+import tensorflow as tf
+from tensorflow.keras.models import Model
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.applications import VGG16
+from tensorflow.keras.activations import relu, softmax
+from tensorflow.keras.layers import Flatten, Dense, Dropout
+from tensorflow.keras.losses import categorical_crossentropy
