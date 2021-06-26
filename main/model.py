25ad0b76730d0a3b715cf4a888efc8411835f548
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2021-06-26 20:15:20 +0430

    Instantiating the model with passing default parameters values

--- main/model.py
+++ main/model.py
@@ -9,3 +9,7 @@ from tensorflow.keras.applications import VGG16
 from tensorflow.keras.activations import relu, softmax
 from tensorflow.keras.layers import Flatten, Dense, Dropout
 from tensorflow.keras.losses import categorical_crossentropy
+
+
+# Instantiating the model with passing default parameters values
+vgg_model = VGG16(weights="imagenet", include_top=False, input_shape=(224, 244, 3))
